0c2f25cdb4eaf6eb0f548dbf87e7e14f096c8270
0123sandeepkumar <speedan0123@gmail.com>
2023-07-30 20:48:13 +0530

    build 500k

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from "vite";
+ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+//import { defineConfig } from 'vite'
+//import vue from '@vitejs/plugin-vue'
+//import react from "@vitejs/plugin-react";
+// import styleImport from 'vite-plugin-style-import'
+// import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  base: '/dist/',
+  build: {
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+        }
+    }
+  }
+})
